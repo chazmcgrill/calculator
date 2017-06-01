7512a0bd70c5ff399fed45d2ac3025bef67963a7
chazmcgrill <charlietaylordev@gmail.com>
2017-06-01 19:10:04 +0100

    click events created
    re factored class names on style and created click events for most of
    the buttons.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,47 @@
 $(document).ready(function(){
-  console.log("**document ready**");
+  console.log('**document ready**');
+
+  // stored value
+  var current = "",
+      stored = 0;
 
   // event listeners
-  $('')
+
+  // top buttons
+  $('.ac-btn').click(function(){
+    console.log('clear button clicked');
+    current = "0";
+    $('.screen').text(current);
+  });
+
+  $('.negate-btn').click(function(){
+    console.log('negate button clicked');
+    if(current.indexOf('-') === -1 && current !== "0"){
+      current = '-' + current;
+      $('.screen').text(current);
+    } else {
+      // REMOVE NEGATE SYMBOL
+      console.log(current.indexOf('-'));
+    }
+  });
+
+  $('.percent-btn').click(function(){
+    console.log('percent button clicked');
+  });
+
+  // number pad
+  $('.num-pad').each(function() {
+    console.log($(this).text());
+    var element = $(this).text();
+    $(this).click(function(){
+      if(current === "0"){
+        current = "";
+      }
+      console.log(element + " button clicked");
+      current += element;
+      $('.screen').text(current);
+      console.log(Number(current));
+    });
+  });
+
 });
