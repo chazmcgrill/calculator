ad4b1d2fc6f30624e1306c7a343246ebb47caa3f
chazmcgrill <charlietaylordev@gmail.com>
2017-06-02 18:49:57 +0100

    Clear Functionality
    Setup clear button and made fully functional. Clear removes the current
    value all clear removes array values.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,15 +5,21 @@ $(document).ready(function() {
 
   // top buttons
   $('.ac-btn').click(function(){
-    console.log('clear button clicked');
-    current = "0";
+    var acStatus = $('.ac-btn').text();
+    if (acStatus === 'C') {
+      console.log('clear button clicked');
+      current = '0';
+      $('.ac-btn').text('AC');
+    } else {
+      console.log('all-clear button clicked');
+      stored = [];
+    }
     $('.screen').text(current);
-    stored = [];
     console.log(stored);
   });
 
   $('.negate-btn').click(function(){
-    if(current.indexOf('-') === -1 && current !== "0"){
+    if (current.indexOf('-') === -1 && current !== "0") {
       current = '-' + current;
     } else {
       current = current.replace('-', '');
@@ -40,13 +46,14 @@ $(document).ready(function() {
   $('.num-pad').each(function() {
     var element = $(this).text();
     $(this).click(function(){
-      if(current === "0" || tempValue){
-        current = "";
+      if(current === '0' || tempValue){
+        current = '';
         tempValue = false;
       }
       console.log(element + " button clicked");
       current += element;
       $('.screen').text(current);
+      $('.ac-btn').text('C');
       console.log(Number(current));
     });
   });
@@ -55,15 +62,14 @@ $(document).ready(function() {
   $('.basic-ops').each(function() {
     var op = $(this).text();
     $(this).click(function() {
-      var opObj = { "−": "-", "×": "*", "÷": '/'}
-      if (op === "−" || op === "×" || op === "÷") {
+      var opObj = { '−': '-', '×': '*', '÷': '/'}
+      if (op === '−' || op === '×' || op === '÷') {
         op = opObj[op];
-        console.log(op);
       }
-      console.log(op + " button clicked");
+      console.log(op + ' button clicked');
       stored.push(current);
       stored.push(op);
-      current = "0";
+      current = '0';
       $('.screen').text(current);
       console.log(stored);
     });
