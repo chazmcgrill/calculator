5cf3d741eebb4c033954b48501bad813ad5338d6
chazmcgrill <charlietaylordev@gmail.com>
2017-06-02 09:42:20 +0100

    Added Operator Functionality
    Added click events for remaining buttons. Setup filter to change
    special characters to normal operators.  Setup storage array for
    result. Equals button setup some errors but mainly working.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   console.log('**document ready**');
 
   // stored value
-  var current = "",
-      stored = 0;
+  var current = "0",
+      stored = [];
 
   // event listeners
 
@@ -12,21 +12,29 @@ $(document).ready(function(){
     console.log('clear button clicked');
     current = "0";
     $('.screen').text(current);
+    stored = [];
   });
 
   $('.negate-btn').click(function(){
-    console.log('negate button clicked');
     if(current.indexOf('-') === -1 && current !== "0"){
       current = '-' + current;
-      $('.screen').text(current);
     } else {
-      // REMOVE NEGATE SYMBOL
-      console.log(current.indexOf('-'));
+      current = current.replace('-', '');
     }
+    $('.screen').text(current);
   });
 
   $('.percent-btn').click(function(){
     console.log('percent button clicked');
+  });
+
+  $('.equals-btn').click(function(){
+    console.log('equals button clicked');
+    stored.push(current);
+    var result = stored.join(' ');
+    console.log(result);
+    current = eval(result);
+    $('.screen').text(current);
   });
 
   // number pad
@@ -41,6 +49,24 @@ $(document).ready(function(){
       current += element;
       $('.screen').text(current);
       console.log(Number(current));
+    });
+  });
+
+  // operator buttons
+  $('.basic-ops').each(function() {
+    console.log($(this).text());
+    var op = $(this).text();
+    $(this).click(function() {
+      var opObj = { "−": "-", "×": "*", "÷": '/'}
+      if (op === "−" || op === "×" || op === "÷") {
+        op = opObj[op];
+        console.log(op);
+      }
+      stored.push(current);
+      stored.push(op);
+      current = "0";
+      $('.screen').text(current);
+      console.log(stored);
     });
   });
 
